0866efa2074d42201c037942a3c90d92f3c0842d
Lucas de Amystherdam <pro.lucas07@gmail.com>
2023-03-08 06:38:34 -0300

    Organizing the house

--- app/helpers/bank_account_helper.rb
+++ app/helpers/bank_account_helper.rb
@@ -1,6 +1,4 @@
 module BankAccountHelper
-  include ActionView::Helpers::NumberHelper
-
   def cent_to_real(cents)
     number_with_precision(cents/100.0, precision: 2, separator: ',', delimiter: '.')
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,42 @@
+puts "Creating user..."
 user = User.create!(
   email: Faker::Internet.email, 
   password: '12345678'
 )
 
+puts "Creating bank agency..."
 bank_agency = BankAgency.create!(
   agency_number: Faker::Number.number(digits: 4), 
   address: Faker::Address.street_address
 )
+puts "Bank agency created..."
 
+puts "Creating bank account..."
 4.times do |i|
   # create variety in the database
   variable_name = "@bank_account_#{i}"
-
+  
   instance_variable_set(variable_name, BankAccount.create!(
     account_number: Faker::Bank.account_number, 
     transaction_limit: Faker::Number.number(digits: 6),
     bank_agency:
   ))
 end
+puts "Bank account created..."
 
+puts "Creating account transactions..."
 6.times do |i|
   # create variety in the database
   i < 4 ? variable_name = "@bank_account_#{i}" : variable_name = "@bank_account_1" 
-
+  
   account_transaction = AccountTransaction.create!(
     transaction_value: Faker::Number.number(digits: 6),
     kind: :deposit, 
     bank_account: instance_variable_get(variable_name),
     user:
   )
-
+  
   transaction_service = TransactionService.new
-  transaction_service.update_account_balance(
-    account_transaction.kind, 
-    account_transaction.transaction_value, 
-    account_transaction.bank_account_id
-  )
+  transaction_service.update_account_balance(account_transaction)
 end
+puts "Account transactions created..."
